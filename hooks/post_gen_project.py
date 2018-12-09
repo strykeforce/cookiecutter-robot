740b294d3eb3938471c996f1d53cb2d633c96220
Jeff Hutchison <jeff@jeffhutchison.com>
2018-12-09 17:07:03 -0500

    Fix permission problem on Windows
    We get an "Access Denied" error when attempting to delete the
    source code file. Thanks Stack Overflow!

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 
 import os
-import shutil
 
 PROJECT_DIR = os.path.realpath(os.path.curdir)
-MAIN_DIR = os.path.join(PROJECT_DIR, 'src', 'main')
-TEST_DIR = os.path.join(PROJECT_DIR, 'src', 'test')
+MAIN_DIR = os.path.join(PROJECT_DIR, "src", "main")
+TEST_DIR = os.path.join(PROJECT_DIR, "src", "test")
+
+import stat
+
+def rmtree(top):
+    for root, dirs, files in os.walk(top, topdown=False):
+        for name in files:
+            filename = os.path.join(root, name)
+            os.chmod(filename, stat.S_IWUSR)
+            os.remove(filename)
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    os.rmdir(top)
+
 
 if __name__ == "__main__":
-    language = 'java' if '{{ cookiecutter.use_kotlin }}' == 'y' else 'kotlin'
-    shutil.rmtree(os.path.join(MAIN_DIR, language))
-    shutil.rmtree(os.path.join(TEST_DIR, language))
+    language = "java" if "{{ cookiecutter.use_kotlin }}" == "y" else "kotlin"
+    rmtree(os.path.join(MAIN_DIR, language))
+    rmtree(os.path.join(TEST_DIR, language))
